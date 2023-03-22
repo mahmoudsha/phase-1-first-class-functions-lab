bd44dbd9fc8298b2dc8fbae545db45190ee8d979
Mahmoud Alshahin <mahmoudshaheen198811@gmail.com>
2023-03-23 00:23:17 +0300

    Done HW function

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Code your solution in this file!
+ const returnFirstTwoDrivers = (arr) =>{
+    const first = [...arr]
+    return [first[0],first[1]]
+ }
+ const returnLastTwoDrivers = (arr)=>{
+    const newArr = [...arr]
+    return newArr.splice(-2,2)
+ }
+
+const selectingDrivers = [returnFirstTwoDrivers,returnLastTwoDrivers]
+
+const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo'];
+const createFareMultiplier = (num)=>{
+    return (fare)=> fare * num ;
+}
+
+const fareDoubler = createFareMultiplier(2);
+const fareTripler = createFareMultiplier(3);
+
+const selectDifferentDrivers = (drivers, f) =>{
+    return f(drivers)
+}
